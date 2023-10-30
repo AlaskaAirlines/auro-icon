e23fbe4ad4c225cf4acd933bb97e0e9f7e7252a9
Dale Sande <dale.sande@gmail.com>
2023-10-30 13:42:58 -0700

    perf: update default URI reference
    This commit updates the default URI from unpkg to jsdelivr as unpkg
    continues to be unstable in most cases.
    
    Changes to be committed:
    modified:   src/auro-icon.js

--- src/auro-icon.js
+++ src/auro-icon.js
@@ -38,7 +38,7 @@ export class AuroIcon extends BaseIcon {
   constructor() {
     super();
 
-    this.uri = 'https://unpkg.com/@alaskaairux/icons@latest/dist';
+    this.uri = 'https://cdn.jsdelivr.net/npm/@alaskaairux/icons@latest/dist';
 
     this.privateDefaults();
   }
