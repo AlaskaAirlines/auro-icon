17f2122fe032a89505ca2cf73a7444d7cc055460
Dale Sande <dale.sande@gmail.com>
2024-01-18 21:27:07 -0800

    feat: add suport for SSR projects #98
    This commit updates scripts and configs to support SSR style projects.
    
    Changes to be committed:
    renamed:    .eslintrc.js -> .eslintrc.cjs
    modified:   package-lock.json
    modified:   package.json
    modified:   packageScripts/postinstall.mjs
    renamed:    scripts/postCss.js -> scripts/postCss.mjs
    new file:   scripts/pre-commit.mjs

--- scripts/postCss.mjs
+++ scripts/postCss.mjs
@@ -1,7 +1,7 @@
-const autoprefixer = require('autoprefixer')
-const postcss = require('postcss')
-const comments = require('postcss-discard-comments')
-const fs = require('fs')
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
+import comments from 'postcss-discard-comments';
+import fs from 'fs';
 
 fs.readFile('src/style.css', (err, css) => {
   postcss([autoprefixer, comments])
@@ -15,4 +15,4 @@ fs.readFile('src/style.css', (err, css) => {
         fs.writeFile('src/style.map', result.map, () => true)
       }
     })
-})
+});
