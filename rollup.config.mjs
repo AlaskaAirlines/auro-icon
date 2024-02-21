53e4ef0315f9ee6fd7b30780e197851a00e79e59
Jason Baker <jason@capsule42.com>
2024-02-20 21:51:04 -0800

    fix(bundle): add auro-alaska.js to the bundler #105

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,9 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import serve from 'rollup-plugin-serve';
 
-const production = !process.env.ROLLUP_WATCH,
+const production = !process.env.ROLLUP_WATCH;
 
- modernConfig = {
+const modernConfig = {
   input: {
     ['auro-icon__bundled']: './index.js',
   },
@@ -21,4 +21,22 @@ const production = !process.env.ROLLUP_WATCH,
   ]
 };
 
-export default [modernConfig];
+const alaskaConfig = {
+  input: {
+    ['auro-alaska__bundled']: './src/auro-alaska.js',
+  },
+  output: {
+    format: 'esm',
+    dir: 'dist/'
+  },
+  plugins: [
+    nodeResolve(),
+    !production &&
+      serve({
+        open: true,
+        openPage: '/docs/'
+      })
+  ]
+};
+
+export default [modernConfig, alaskaConfig];
