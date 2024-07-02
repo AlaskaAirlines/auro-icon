e0a1ca59355d2d68cc9e0de060afb8d2b64797f4
Jordan Jones <jmjones2463@gmail.com>
2024-07-02 10:50:48 -0700

    fix(label): add classmap for label attribute

--- src/auro-icon.js
+++ src/auro-icon.js
@@ -179,8 +179,13 @@ export class AuroIcon extends BaseIcon {
       'util_displayHiddenVisually': !this.label
     };
 
+    const classes = {
+      'label': this.label
+    };
+
     return html`
       <div
+        class="${classMap(classes)}"
         title="${ifDefined(this.title ? this.title : undefined)}"
         >
 
